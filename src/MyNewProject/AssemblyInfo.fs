5859c2fd1f5306618f8ec67c3aee3e5a47e4ccdd
Julien Roncaglia <julien@roncaglia.fr>
2017-02-08 22:58:54 +0100

    Fixup paket

---
+++ src/MyNewProject/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("MyNewProject")>]
+[<assembly: AssemblyProductAttribute("MyNewProject")>]
+[<assembly: AssemblyDescriptionAttribute("MyNewProject")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] InformationalVersion = "0.0.1"
